79aa4e436d91e9ee30b9b2dff0c699b0770e658c
Chris <tchris96@vt.edu>
2018-03-13 21:00:02 -0400

    Debugged Part 1 c to give correct thread initialization.  If late submission isn't acceptable use older version of submission

--- L15/opi.c
+++ L15/opi.c
@@ -6,12 +6,13 @@
 
 int main(int argc, char **argv) {
 
+  //long long Ncircle = 0;
   //seed random number generator
   // Q2b: get the number of threads to run with from agrv and 
   // add OpenMP API code to set number of threads here
   int Nthreads = 1;
 
-  Nthreads = atoi(argv[0]); 
+  Nthreads = (int)atoi(argv[0]); 
  
   omp_set_num_threads(Nthreads);
 
@@ -21,54 +22,60 @@ int main(int argc, char **argv) {
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
 
- // int rank = omp_get_thread_num();
- // double seed = srand48_r(rank);
-  long long int Ntotal = 0;
-  long long int Ncircle = 0;
+  // int rank = omp_get_thread_num();
+  // double seed = srand48_r(rank);
+ /// long long int Ntotal = 0;
+  //long long int Ncircle = 0;
+  long long int Ntrials = 10000000;
 
   double start_time = omp_get_wtime();
-  #pragma omp parallel
+  #pragma omp parallel for
+  for(int n=0; n<Ntrials; n++)
   {
   long int seed = 0;
   int rank = omp_get_thread_num();
-  seed = srand48_r(rank, drandData+0);
-  srand48_r(seed, drandData+0);
-
-  long long int Ntrials = 10000000;
+  seed = rank;
+  srand48_r(seed, drandData+rank);  
 
   //need running tallies
  // long long int Ntotal=0;
  // long long int Ncircle=0;
-
-
-  #pragma omp for reduction(+:Ncircle)
+  }
+// printf("test");
+  long long int Ncircles = 0;
+  long long int Ntotal = 0;
+//  printf("a");
+  #pragma omp parallel for reduction(+:Ncircles)
   for (long long int n=0; n<Ntrials; n++) {
     double rand1;
     double rand2;
-
+//  printf("b");
+  int rank = omp_get_thread_num();
     //gererate two random numbers (use the thread id to offset drandData)
-    drand48_r(drandData+0, &rand1);
-    drand48_r(drandData+0, &rand2);
+    drand48_r(drandData+rank, &rand1);
+    drand48_r(drandData+rank, &rand2);
     
     double x = -1 + 2*rand1; //shift to [-1,1]
     double y = -1 + 2*rand2;
 
 
     //check if its in the circle
-    if (sqrt(x*x+y*y)<=1) Ncircle++;
+    if (sqrt(x*x+y*y)<=1) Ncircles++;
     Ntotal++;
 
     if (n%100 ==0) {
-      double pi = 4.0*Ncircle/ (double) (n);
+      double pi = 4.0*Ncircles/ (double) (n);
       printf("Our estimate of pi is %g \n", pi);
     }
+//  printf("c");
   }
-  double pi = 4.0*Ncircle/ (double) (Ntotal);
+
+  //return Ncircle;
+  double pi = 4.0*Ncircles/ (double) (Ntotal);
   printf("Our final estimate of pi is %g \n", pi);
 
   //printf("The time required is %n \n", time);
   free(drandData);  
-  }
   double time = omp_get_wtime() - start_time;
   printf("The time required is %f \n", time);
 
